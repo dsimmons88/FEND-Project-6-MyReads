0c008f5fb91a663a7ab312544d1d2468f44761f5
Dwaine Simmons <dwainesimmons88@gmail.com>
2018-10-01 19:57:56 -0400

    Now I can move books to different shelves

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,6 +6,7 @@ import * as BooksAPI from '../BooksAPI'
 class Book extends React.Component {
 
 
+
   render() {
     return (
       <li>
@@ -13,7 +14,7 @@ class Book extends React.Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail}")`  }}></div>
            <div className="book-shelf-changer">
-              <select value={this.props.book.shelf || "none"} onChange={(e) => {this.props.updateBook(this.props.book, e.target.value) }}>
+              <select value={this.props.book.shelf || "none"} onChange={(e) => ( this.props.updateBook(this.props.book, e.target.value) )}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -34,18 +35,3 @@ class Book extends React.Component {
 }
 
 export default Book;
-
-
-/*
-<div className="book-shelf-changer">
-  <select className="u-full-width" value={this.state.book.shelf}
-   onChange={this.handleChange}>
-    <option value="move" disabled>Move to...</option>
-    <option value="currentlyReading">Currently Reading</option>
-    <option value="wantToRead">Want to Read</option>
-    <option value="read">Read</option>
-    <option value="none">None</option>
-
-</div>
-</select>
-*/
